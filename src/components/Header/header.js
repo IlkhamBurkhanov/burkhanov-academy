192ee43bf190d0ac04149d4b6cb0d6eab0b60bda
IlkhamBurkhonov <ilkhomjonburkhonov207@gmail.com>
2023-09-20 11:30:16 +0500

    add some sections

---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className=" bg-emerald-800 py-3">
+      <div className=" flex justify-center">
+        <div className=" text-white text-2xl font-semibold">
+          Burkhanov Academy
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
